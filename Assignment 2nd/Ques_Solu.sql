b805e31e2cb64923117efe5eda892da6318fdff2
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-12-12 16:04:28 +0530

    Update Ques_Solu.sql

--- Assignment 2nd/Ques_Solu.sql
+++ Assignment 2nd/Ques_Solu.sql
@@ -3,14 +3,19 @@ SELECT ID,tot_cred FROM `student` WHERE ID = 'S101';
 
 -- Q-2.2 Find the grade point average (GPA) for the above student, that is, the total grade points divided by the total credits for the associated courses ?
 
-SELECT std.ID,std.Name,
-  COALESCE(SUM(gp.points * crs.credits) / NULLIF(SUM(crs.credits), 0), 0) AS GP
-  FROM student std
-    LEFT JOIN takes tk ON std.ID = tk.ID
-      LEFT JOIN grade_points gp ON tk.grade = gp.grade
-        LEFT JOIN course crs ON tk.course_id = crs.course_id
-          GROUP BY std.ID, std.Name 
-            HAVING std.ID = 'S101';
+SELECT std.ID,
+       std.Name,
+       CASE
+           WHEN SUM(crs.credits) = 0 THEN 0
+           ELSE SUM(gp.points * crs.credits) / SUM(crs.credits)
+       END AS GP
+FROM student std
+LEFT JOIN takes tk ON std.ID = tk.ID
+LEFT JOIN grade_points gp ON tk.grade = gp.grade
+LEFT JOIN course crs ON tk.course_id = crs.course_id
+GROUP BY std.ID, std.Name
+HAVING std.ID = 'S101';
+
 
 -- Q-2.3 -- Find the ID and the grade-point average of each student ?
 
